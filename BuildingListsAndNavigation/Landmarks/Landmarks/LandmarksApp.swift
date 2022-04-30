a3e4cae4486f37c3ee6252b94051a82d8865c34a
Arvin Shen <ashen3292@gmail.com>
2022-04-29 23:06:21 -0700

    Add Building Lists and Navigation tutorial

---
+++ BuildingListsAndNavigation/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Arvin Shen on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
