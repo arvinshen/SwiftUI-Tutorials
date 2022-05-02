e46cd2bb489b7a38d395ba81785f68ffcda1272e
Arvin Shen <ashen3292@gmail.com>
2022-05-02 13:11:54 -0700

    Add Animating Views and Transitions tutorial

---
+++ AnimatingViewsAndTransitions/Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Arvin Shen on 4/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
